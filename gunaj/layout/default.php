13546413a4caac94e91aedc8613c17ddf10588d5
Pablo Velaquez <pablocho88@gmail.com>
2018-03-21 10:05:32 -0300

    HOME: Botones en el hover mantener siempre la logica del color del contexto

--- gunaj/layout/default.php
+++ gunaj/layout/default.php
@@ -97,7 +97,7 @@ echo $OUTPUT->doctype() ?>
 </nav>
 
 <header id="moodleheader" class="clearfix">
-    <div id="page-navbar" class="row">
+    <div id="page-navbar" class="container">
         <nav class="breadcrumb-nav" role="navigation" aria-label="breadcrumb"><?php echo $OUTPUT->navbar(); ?></nav>
         <div class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></div>
     </div>
